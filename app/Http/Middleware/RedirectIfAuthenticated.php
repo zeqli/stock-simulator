9a06a8b9f32b95dc21faac423bed7db1cb5aaad9
zeqli <l.caecar@gmail.com>
2016-11-03 23:27:57 -0500

    change default authenticaed redirect path

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
+// 'guest' Middleware 
 class RedirectIfAuthenticated
 {
     /**
@@ -18,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/simulator/home');
         }
 
         return $next($request);
