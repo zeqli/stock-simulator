d7755968717503a3d52bb6c08d12ba12f29b868e
zeqli <l.caecar@gmail.com>
2016-11-25 20:50:49 -0600

    Modify seed data

--- database/seeds/HoldTableSeeder.php
+++ database/seeds/HoldTableSeeder.php
@@ -15,13 +15,27 @@ class HoldTableSeeder extends Seeder
 
         //
         #DB::table('trade')->truncate();
-        DB::table('hold')->insert([
-            'U_id' => '1',
-            'symbol' => 'NASDAQ',
-            'quantity' => '5'
-            
-        ]);
-        factory(App\hold::class, 50)->create();
+        // DB::table('hold')->insert([
+        //     'U_id' => '1',
+        //     'symbol' => 'GOOG',
+        //     'quantity' => '5'
+        // ]);
+        // factory(App\hold::class, 50)->create();
     
+        $query = DB::table('accounts')
+                    ->join('trade', 'trade.U_id', '=','accounts.id')
+                    ->select('accounts.id as U_id', 'trade.symbol as symbol', DB::raw('SUM(trade.quantity) as qty'))
+                    ->where('trade.status', '=' ,'done')
+                    ->groupBy('accounts.id', 'trade.symbol')
+                    ->get();
+
+        $hold_arr = $query->toArray();
+        foreach ($hold_arr as $entry ) {
+            $hold = new App\hold;
+            $hold->U_id = $entry->U_id;
+            $hold->symbol = $entry->symbol;
+            $hold->quantity = $entry->qty;
+            $hold->save();
+        }
     }
 }
