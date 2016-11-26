789d29aa2adfb999aa8736dd796b6b2c202150c6
zeqli <l.caecar@gmail.com>
2016-11-25 20:44:38 -0600

    Optimize YahooStock api

--- app/Mylibrary/YahooStock.php
+++ app/Mylibrary/YahooStock.php
@@ -48,29 +48,53 @@ class YahooStock {
     {        
         $result = array();        
         $format = $this->format;
-        
-        foreach ($this->stocks as $stock)
-        {            
-            /**
-             * fetch data from Yahoo!
-             * s = stock code
-             * f = format
-             * e = filetype
-             */
-            $handle = fopen("http://finance.yahoo.com/d/quotes.csv?s=$stock&f=$format&e=.csv",'r');
-   //         $s = file_get_contents("http://finance.yahoo.com/d/quotes.csv?s=$stock&f=$format&e=.csv");
-            $data = fgetcsv($handle);
 
-            /** 
-             * convert the comma separated data into array
-             */
-    //        $data = explode( ',', $s);
+
+        $tickers = implode(',', $this->stocks);
+        $handle = fopen("http://finance.yahoo.com/d/quotes.csv?s=$tickers&f=$format",'r');
+
+        $row = 0;
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $sym = $this->stocks[$row++];
+            $result[$sym] = $data;
+        };
+
+        
+   //      foreach ($this->stocks as $stock)
+   //      {            
+   //          /**
+   //           * fetch data from Yahoo!
+   //           * s = stock code
+   //           * f = format
+   //           * e = filetype
+   //           */
+   //          $handle = fopen("http://finance.yahoo.com/d/quotes.csv?s=$stock&f=$format",'r');
+   // //         $s = file_get_contents("http://finance.yahoo.com/d/quotes.csv?s=$stock&f=$format&e=.csv");
+   //          $data = fgetcsv($handle);
+
+   //          /** 
+   //           * convert the comma separated data into array
+   //           */
+   //  //        $data = explode( ',', $s);
             
-            /** 
-             * populate result array with stock code as key
-             */
-            $result[$stock] = $data;
-        }
+   //          /** 
+   //           * populate result array with stock code as key
+   //           */
+   //          $result[$stock] = $data;
+   //      }
         return $result;
+    }
+
+    public function getTotalValue($stocks_with_qty){
+        $this->addFormat("l1");    // l1 - last trade price 
+        foreach ($stocks_with_qty as $symbol => $qty) {
+            $this->addStock($symbol);
+        }
+        $result = $this->getQuotes();
+        $total_value = 0.0;
+        foreach ($stocks_with_qty as $symbol => $qty) {
+            $total_value += $result[$symbol][0] * $qty;
+        }
+        return $total_value;
     }
 } 
